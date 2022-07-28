3400fa64cc0e95852099b5ffbf2f3f252e24aa68
VENKATESWARI NANDYALA <venkateswarinandyala@gmail.com>
2022-07-28 23:18:06 +0530

    First Commit

---
+++ Joes_Pizza_Shop/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Joes_Pizza_Shop.Models
+{
+    public partial class Order
+    {
+      
+        public Order()
+        {
+            this.CustomerOrderTables = new HashSet<CustomerOrderTable>();
+        }
+
+        [Key]
+        public int OrderID { get; set; }
+        public Nullable<System.DateTime> OrderDate { get; set; }
+        public Nullable<double> TotalAmount { get; set; }
+
+       
+        public virtual ICollection<CustomerOrderTable> CustomerOrderTables { get; set; }
+    }
+}
